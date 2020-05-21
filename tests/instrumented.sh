e44834a69f8e75665338f8f8383218b8515a85fe
Joseph Ellis <jhellis3@gmail.com>
2020-05-21 12:43:33 -0500

    Reduced Tests
    Bench: 9545634

--- tests/instrumented.sh
+++ tests/instrumented.sh
@@ -17,26 +17,12 @@ case $1 in
     postfix='1>/dev/null'
     threads="1"
   ;;
-  --valgrind-thread)
-    echo "valgrind-thread testing started"
-    prefix=''
-    exeprefix='valgrind --error-exitcode=42'
-    postfix='1>/dev/null'
-    threads="2"
-  ;;
   --sanitizer-undefined)
     echo "sanitizer-undefined testing started"
     prefix='!'
     exeprefix=''
     postfix='2>&1 | grep -A50 "runtime error:"'
     threads="1"
-  ;;
-  --sanitizer-thread)
-    echo "sanitizer-thread testing started"
-    prefix='!'
-    exeprefix=''
-    postfix='2>&1 | grep -A50 "WARNING: ThreadSanitizer:"'
-    threads="2"
 
 cat << EOF > tsan.supp
 race:TTEntry::move
