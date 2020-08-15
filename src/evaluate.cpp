a728ba6f9b76a4fe4507a27e555148519e2aa1ff
Joseph Ellis <jhellis3@gmail.com>
2020-08-15 15:44:47 -0500

    Retire NNUEThreshold
    Bench: 9711876

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -115,7 +115,6 @@ namespace {
   constexpr Value LazyThreshold1 =  Value(1400);
   constexpr Value LazyThreshold2 =  Value(1300);
   constexpr Value SpaceThreshold = Value(12222);
-  constexpr Value NNUEThreshold  =   Value(575);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 81, 52, 44, 10 };
