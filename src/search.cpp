c1e6d9c5718f1425730025dc4cdc094b4726904d
Joseph Ellis <jhellis3@gmail.com>
2021-01-14 11:30:48 -0600

    Remove LMR Condition
    Bench: 4480137

--- src/search.cpp
+++ src/search.cpp
@@ -1159,7 +1159,6 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
-              || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
