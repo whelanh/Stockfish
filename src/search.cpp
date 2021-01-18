7f07a744c0ac474883cb395c90ca68dbe57149f0
Joseph Ellis <jhellis3@gmail.com>
2021-01-18 12:07:32 -0600

    Skip futility prune when ttValue < eval
    Bench: 3781628

--- src/search.cpp
+++ src/search.cpp
@@ -832,6 +832,7 @@ namespace {
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
         &&  depth < 9
+        && (!ss->ttHit || ttValue >= eval)
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
