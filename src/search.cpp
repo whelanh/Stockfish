153fc2542cb109d144a3f114ec7322c263a8c4a0
Joseph Ellis <jhellis3@gmail.com>
2021-08-03 09:58:37 -0500

    LMR adjustment
    Bench: 8419151

--- src/search.cpp
+++ src/search.cpp
@@ -1150,7 +1150,7 @@ namespace {
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode)
+          if (PvNode || (ss-1)->moveCount == 1)
               r--;
 
           // Decrease reduction if the ttHit running average is large
@@ -1191,9 +1191,12 @@ namespace {
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
-          Depth rr = newDepth / (2 + ss->ply / 2.8);
+          if (!PvNode && (ss-1)->moveCount > 1)
+          {
+            Depth rr = newDepth / (2 + ss->ply / 2.8);
 
-          r -= rr;
+            r -= rr;
+          }
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
