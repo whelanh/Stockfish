11b3945eddf2660dfc8ef066cf0f369bb91e6289
Joseph Ellis <jhellis3@gmail.com>
2021-10-13 20:54:46 -0500

    If we expected to fail high but don't, extend subsequent moves.
    Bench: 6016723

--- src/search.cpp
+++ src/search.cpp
@@ -1125,7 +1125,12 @@ moves_loop: // When in check, search starts here
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
+          {
               extension = -2;
+
+              // If we don't fail high, extend subsequent moves
+              depth += 1;
+          }
       }
 
       // Capture extensions for PvNodes and cutNodes
