c4219b6764e2dc652707c04c72f21cd90598e1f7
Joseph Ellis <jhellis3@gmail.com>
2021-11-14 13:18:22 -0600

    Create general solution for potential good nodes
    Bench: 5024818

--- src/search.cpp
+++ src/search.cpp
@@ -978,12 +978,10 @@ moves_loop: // When in check, search starts here
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
 
-    // Indicate PvNodes that will probably fail low if the node was searched
-    // at a depth equal or greater than the current depth, and the result of this search was a fail low.
-    bool likelyFailLow =    PvNode
-                         && ttMove
-                         && (tte->bound() & BOUND_UPPER)
-                         && tte->depth() >= depth;
+    // Indicate potentially good Nodes.
+    bool goodNode =    ttMove
+                    && ttValue >= alpha
+                    && (tte->bound() & BOUND_LOWER);
 
     // Step 12. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1176,7 +1174,7 @@ moves_loop: // When in check, search starts here
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
-              && !likelyFailLow)
+              && goodNode)
               r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
